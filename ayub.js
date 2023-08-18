c1d9f77fc38e2e1a571c4ca250dde6077e3ee57b
Anggun <anggundina354@gmail.com>
2023-08-18 21:13:45 +0700

    Menambahkan alamat

--- ayub.js
+++ ayub.js
@@ -1,7 +1,8 @@
-function kirim(){
+function kirim() {
     var id_name = document.getElementById("name")
     var id_nim = document.getElementById("nim")
     var id_prodi = document.getElementById("prodi")
     var id_tampil = document.getElementById("tampil")
-    id_tampil.innerText = id_name.value + ", " + id_nim.value +", " + id_prodi.value
+    var id_alamat = document.getElementById("alamat")
+    id_tampil.innerText = id_name.value + ", " + id_nim.value + ", " + id_prodi.value + ", " + id_alamat.value
 }
